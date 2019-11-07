fe1a5ed4ded7d15752077e69f4195d93ced08bb9
Gerhard Herbert <gerhard.herbert@avira.com>
2019-11-07 20:51:41 +0100

    added deposit and withdraw

--- lib/account.dart
+++ lib/account.dart
@@ -2,4 +2,7 @@ library banking;
 
 class Account {
   double balance = 0;
+
+  void deposit(int amount) => balance += amount;
+  void withdraw(int amount) => balance -= amount;
 }
--- test/account_test.dart
+++ test/account_test.dart
@@ -3,8 +3,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:banking/account.dart';
 
 void main() {
+  var account = Account();
+
+  setUp((){
+    account = Account();
+  });
+
   test('initial account has balance == 0', () {
-    final account = Account();
     expect(0, account.balance);
+  });
+
+  test('deposit amount increases balance', () {
+    account.deposit(1000);
+    expect(1000, account.balance);
+  });
+  test('withdraw amount decreases balance', () {
+    account.deposit(1000);
+    account.withdraw(500);
+    expect(500, account.balance);
   });
 }
